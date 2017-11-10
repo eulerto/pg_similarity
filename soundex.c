eulerto/pg_similarity

commit adacf8ab8f6ed8304ff4c5dcffbf0378656091e1

Fix outstanding defects.
Spotted by Coverity.

soundex.c
@@ -53,10 +53,10 @@ static char *_soundex(char *a)
 	scode[PGS_SOUNDEX_LEN] = '\0';
 
 	/* ignoring non-alpha characters */
-	while (!isalpha(*a) && a)
+	while (!isalpha(*a) && *a != '\0')
 		a++;
 
-	if (!a)
+	if (*a != '\0')
 		elog(ERROR, "string doesn't contain non-alpha character(s)");
 
 	/* get the first letter */

levenshtein.c
@@ -156,7 +156,7 @@ int _lev_slow(char *a, char *b, int icost, int dcost)
 	for (i = 0; i <= alen; i++)
 	{
 		matrix[i] = (int *) malloc((blen + 1) * sizeof(int));
-		if (matrix == NULL)
+		if (matrix[i] == NULL)
 			elog(ERROR, "memory exaushted for array size %d", (blen + 1));
 	}
 

mongeelkan.c
@@ -45,7 +45,7 @@ static double _mongeelkan(char *a, char *b)
 	for (i = 0; i <= alen; i++)
 	{
 		matrix[i] = (float *) malloc((blen + 1) * sizeof(float));
-		if (matrix == NULL)
+		if (matrix[i] == NULL)
 			elog(ERROR, "memory exaushted for array size %d", blen);
 	}
 

smithwaterman.c
@@ -97,7 +97,7 @@ static double _smithwaterman(char *a, char *b)
 	for (i = 0; i <= alen; i++)
 	{
 		matrix[i] = (float *) malloc((blen + 1) * sizeof(float));
-		if (matrix == NULL)
+		if (matrix[i] == NULL)
 			elog(ERROR, "memory exaushted for array size %d", blen);
 	}
 

smithwatermangotoh.c
@@ -40,7 +40,7 @@ static double _smithwatermangotoh(char *a, char *b)
 	for (i = 0; i <= alen; i++)
 	{
 		matrix[i] = (float *) malloc((blen + 1) * sizeof(float));
-		if (matrix == NULL)
+		if (matrix[i] == NULL)
 			elog(ERROR, "memory exaushted for array size %d", blen);
 	}
 

commit 36d6f56554d09089331fe6d4523be4a6ade824ef

Fix soundex non-alpha detection
adacf8ab8f6ed8304ff4c5dcffbf0378656091e1 broke the non-alpha character
string parsing by inverting the logic.

soundex.c
@@ -56,7 +56,7 @@ static char *_soundex(char *a)
 	while (!isalpha(*a) && *a != '\0')
 		a++;
 
-	if (*a != '\0')
+	if (*a == '\0')
 		elog(ERROR, "string doesn't contain non-alpha character(s)");
 
 	/* get the first letter */