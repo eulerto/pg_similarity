50d0b02d18bcd858a76b9885388463786f837085
Euler Taveira <euler@timbira.com.br>
2018-03-29 23:02:22 -0300

    Remove Windows workaround
    Since commit 77e50a61ffff34aae8cb3ffa5a7333c1049e5f37 this workaround
    was included in PostgreSQL.

--- similarity.h
+++ similarity.h
@@ -7,47 +7,6 @@
 #include "utils/builtins.h"
 #include "utils/guc.h"
 
-/*
- * XXX Windows workaround
- */
-#ifndef WIN32
-
-#define PGS_EXPORT
-
-#else
-
-#define PGS_EXPORT		__declspec(dllexport)
-
-/*
- * PG_MODULE_MAGIC and PG_FUNCTION_INFO_V1 macros seems to be broken.
- * It uses PGDLLIMPORT, but those objects are not imported from postgres
- * and exported from the user module. So, it should be always dllexported.
- */
-
-#undef PG_MODULE_MAGIC
-#define PG_MODULE_MAGIC \
-extern PGS_EXPORT const Pg_magic_struct *PG_MAGIC_FUNCTION_NAME(void); \
-const Pg_magic_struct * \
-PG_MAGIC_FUNCTION_NAME(void) \
-{ \
-	static const Pg_magic_struct Pg_magic_data = PG_MODULE_MAGIC_DATA; \
-	return &Pg_magic_data; \
-} \
-extern int no_such_variable
-
-#undef PG_FUNCTION_INFO_V1
-#define PG_FUNCTION_INFO_V1(funcname) \
-extern PGS_EXPORT const Pg_finfo_record * CppConcat(pg_finfo_,funcname)(void); \
-const Pg_finfo_record * \
-CppConcat(pg_finfo_,funcname) (void) \
-{ \
-	static const Pg_finfo_record my_finfo = { 1 }; \
-	return &my_finfo; \
-} \
-extern int no_such_variable
-
-#endif	/* Windows workaround */
-
 
 /* case insensitive ? */
 #define		PGS_IGNORE_CASE			1
@@ -204,43 +163,43 @@ void _PG_init(void);
 /*
  * external function declarations
  */
-extern Datum PGS_EXPORT block(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT block_op(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT cosine(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT cosine_op(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT dice(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT dice_op(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT euclidean(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT euclidean_op(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT hamming(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT hamming_op(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT hamming_text(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT hamming_text_op(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT jaccard(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT jaccard_op(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT jaro(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT jaro_op(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT jarowinkler(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT jarowinkler_op(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT lev(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT lev_op(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT levslow(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT levslow_op(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT matchingcoefficient(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT matchingcoefficient_op(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT mongeelkan(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT mongeelkan_op(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT needlemanwunsch(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT needlemanwunsch_op(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT overlapcoefficient(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT overlapcoefficient_op(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT qgram(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT qgram_op(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT smithwaterman(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT smithwaterman_op(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT smithwatermangotoh(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT smithwatermangotoh_op(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT soundex(PG_FUNCTION_ARGS);
-extern Datum PGS_EXPORT soundex_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT block(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT block_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT cosine(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT cosine_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT dice(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT dice_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT euclidean(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT euclidean_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT hamming(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT hamming_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT hamming_text(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT hamming_text_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT jaccard(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT jaccard_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT jaro(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT jaro_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT jarowinkler(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT jarowinkler_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT lev(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT lev_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT levslow(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT levslow_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT matchingcoefficient(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT matchingcoefficient_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT mongeelkan(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT mongeelkan_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT needlemanwunsch(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT needlemanwunsch_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT overlapcoefficient(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT overlapcoefficient_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT qgram(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT qgram_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT smithwaterman(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT smithwaterman_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT smithwatermangotoh(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT smithwatermangotoh_op(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT soundex(PG_FUNCTION_ARGS);
+extern Datum PGDLLEXPORT soundex_op(PG_FUNCTION_ARGS);
 
 #endif
